c1d86e25b91f43b7f0d984391017292cbb54613b
ta3nich <118207850+ta3nich@users.noreply.github.com>
2022-11-15 02:12:06 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ LOG="/var/log/mysql/error.log"
 
 # Set permission of config file
 chmod 644 ${CONF_FILE}
-chmod 644 /etc/mysql/conf.d/mysqld_charset.cnf
+#chmod 644 /etc/mysql/conf.d/mysqld_charset.cnf
 
 StartMySQL ()
 {
