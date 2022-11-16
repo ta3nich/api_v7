fb0ab4aec27cdcd4e8babbdcf936c4cbf0e7401e
GH0STAV0 <you@example.com>
2022-11-16 06:43:59 -0700

    docker with node

--- start_mysql.sh
+++ start_mysql.sh
@@ -6,9 +6,12 @@ service mysql start
 
 #mysql -uroot -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '$PASS'"
 #mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION"
+mysql -uroot -e "create database b0t"
+mysql -uroot -pPwd123 b0t < all.sql
+
 mysql -uroot -e "CREATE USER 'rooti'@'%' IDENTIFIED WITH mysql_native_password  BY 'Pwd123'"
 mysql -uroot -e "GRANT ALL ON *.* TO 'root'@'%'"
-mysql -uroot -pPwd123 mysql -e "source last_sql.sql"
+#mysql -uroot -pPwd123 mysql -e "source last_sql.sql"
 mysql -uroot -e "CREATE USER 'monty'@'127.0.0.1' IDENTIFIED BY 'Pwd123'"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'monty'@'127.0.0.1' WITH GRANT OPTION "
 mysql -uroot -e "GRANT ALL ON *.* TO root@'%' IDENTIFIED BY 'Pwd123' WITH GRANT OPTION; FLUSH PRIVILEGES"
