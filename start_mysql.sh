04a9529a08061e0694524bb171100729806cbb07
GH0STAV0 <you@example.com>
2022-11-16 05:34:05 -0700

    docker with node

--- start_mysql.sh
+++ start_mysql.sh
@@ -1,5 +1,5 @@
 service mysql start
-echo "=> Creating MySQL user ${MYSQL_USER} with ${_word} password"
+#echo "=> Creating MySQL user ${MYSQL_USER} with ${_word} password"
 #mysql -uroot -pPwd123 mysql -e "source last_sql.sql"
 #echo "CREATE USER 'root'@'localhost'" | mysql -u root
 
@@ -8,7 +8,7 @@ echo "=> Creating MySQL user ${MYSQL_USER} with ${_word} password"
 #mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION"
 mysql -uroot -e "CREATE USER 'root'@'%' IDENTIFIED BY 'Pwd123'"
 mysql -uroot -e "GRANT ALL ON *.* TO 'root'@'%'"
-#mysql -uroot -pPwd123 mysql -e "source last_sql.sql"
+mysql -uroot -pPwd123 mysql -e "source last_sql.sql"
 mysql -uroot -e "CREATE USER 'monty'@'127.0.0.1' IDENTIFIED BY 'Pwd123'"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'monty'@'127.0.0.1' WITH GRANT OPTION "
 mysql -uroot -e "GRANT ALL ON *.* TO root@'%' IDENTIFIED BY 'Pwd123' WITH GRANT OPTION; FLUSH PRIVILEGES"
