90a3f9cbaeba902a95b950d17285c5c5a272255d
GH0STAV0 <you@example.com>
2022-11-16 06:20:21 -0700

    docker with node

--- start_mysql.sh
+++ start_mysql.sh
@@ -2,11 +2,11 @@ service mysql start
 #echo "=> Creating MySQL user ${MYSQL_USER} with ${_word} password"
 #mysql -uroot -pPwd123 mysql -e "source last_sql.sql"
 #echo "CREATE USER 'root'@'localhost'" | mysql -u root
-
+# ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';.
 
 #mysql -uroot -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '$PASS'"
 #mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION"
-mysql -uroot -e "CREATE USER 'root'@'%' IDENTIFIED BY 'Pwd123'"
+mysql -uroot -e "CREATE USER 'rooti'@'%' IDENTIFIED WITH mysql_native_password  BY 'Pwd123'"
 mysql -uroot -e "GRANT ALL ON *.* TO 'root'@'%'"
 mysql -uroot -pPwd123 mysql -e "source last_sql.sql"
 mysql -uroot -e "CREATE USER 'monty'@'127.0.0.1' IDENTIFIED BY 'Pwd123'"
